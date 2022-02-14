68a4b39493cb9c5b9ac80931ddb70391b45e1c79
prihissung <prihissung@hotmail.com>
2022-02-13 21:15:25 -0300

    inicial front

---
+++ Downloads/Cursos-master/Cursos-master/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Cursos.Data;
+using Cursos.Models;
+using Cursos.Models.Entidades;
+using Cursos.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Cursos.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Context _context;
+        private readonly IMapper _mapper;
+
+        public HomeController(Context context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            HomeViewModel homeViewModel = new HomeViewModel();
+
+            List<Curso> cursos = _context.Curso.ToList();
+            List<PostagemBlog> postagens = _context.PostagemBlogs.ToList();
+
+            homeViewModel.Cursos = _mapper.Map<List<CursoViewModel>>(cursos);
+            homeViewModel.Postagens = _mapper.Map<List<PostagemBlogViewModel>>(postagens);
+
+            return View(homeViewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Downloads/Cursos-master/Cursos-master/Migrations/20220212020136_projeto_gama.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cursos.Migrations
+{
+    public partial class projeto_gama : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
